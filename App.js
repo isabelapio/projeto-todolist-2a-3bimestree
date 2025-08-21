826a56825689754c2d78c90ff76b235c0e4d62fa
Melânia Gabrielli Silva Amaral <melaniagabriellis@gmail.com>
2025-08-21 11:58:56 -0300

    [feat] Input Tarefa

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import logo from './assets/pngegg (14).png';
+import {useState}  from "react";
+import btnAdd from "pngegg(15).png"
 
 export default function App() {
+const [tarefa, setTarefa]= useState("")
   return (
     <View style={styles.container}>
       <View>
-        <Image source={logo} style={styles.logo}/> </View>
-      <Text>Isabela Pio</Text>
+        <Image source={logo} style={styles.logo}/> 
+        <Text>TODO List</Text>
+        </View>
+      <View>
+        <TextInput
+         placeholder="Entre com a tarefa"
+        value={tarefa}
+        onChangeText={setTarefa}
+        />
+        <Image source={btnAdd}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
